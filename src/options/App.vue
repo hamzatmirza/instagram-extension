83b68b991e13ee43e5b379b0dfc175f11f9c51ce
Hamza Mirza <hamza.tariq.mirza@gmail.com>
2021-06-16 01:41:09 +0500

    Initial

---
+++ src/options/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <b-button @click="testAPI" variant="success">Button</b-button>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      msg: 'Welcome!'
+    }
+  },
+  methods:{
+    testAPI(){
+     axios.get("https://www.instagram.com/hamza.t.mirza/?__a=1").then(function(response){
+        console.log(response);
+     });
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
